afa80f3727a76a11a08edd0a33fb528d96af9b26
Tugbay Atilla <tatilla@igus.net>
2023-11-20 13:39:00 +0100

    initial commit

---
+++ src/page-replacement-clock-6kyu.spec.ts
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import 'mocha';
+
+describe('name of  ', 
+  () => { 
+    it('should ', () => { 
+      expect(true).to.equal(true); 
+  }); 
+});
---
+++ src/page-replacement-clock-6kyu.ts
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/62f23d84eb2533004be50c0d/train/typescript
+
+export function clock(n: number, referenceList: number[]): number[] {
+    return [];
+}
